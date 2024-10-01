b86d6902a7dcf1b5055c9c63411441fc291e891f
Raitis Rotgalvis <raitis.rotgalvis@gmail.com>
2024-10-01 22:21:39 +0300

    Implemented logo variants

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
   return (
     <div className="flex h-dvh items-center justify-center">
       <Box>
-        <Logo />
+        <Logo type="large" />
         <div className="flex flex-col items-stretch gap-2">
           <Button>Browse goblins</Button>
           <Button>Find nearest goblin</Button>
--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,8 +1,36 @@
-function Logo() {
+type LogoProps = {
+  className?: string;
+  type?: "regular" | "small" | "large" | "icon";
+};
+
+const styles: Record<NonNullable<LogoProps["type"]>, Record<string, string>> = {
+  regular: {
+    container: "gap-2",
+    img: "h-12 w-12",
+    text: "text-3xl",
+  },
+  small: {
+    container: "gap-1",
+    img: "h-8 w-8",
+    text: "text-xl",
+  },
+  large: {
+    container: "gap-3",
+    img: "h-24 w-24",
+    text: "text-6xl",
+  },
+  icon: {
+    container: "gap-0",
+    img: "h-6 w-6",
+    text: "hidden",
+  },
+};
+
+function Logo({ className = "", type = "regular" }: LogoProps) {
   return (
-    <div className="mx-auto my-0 flex items-center justify-center gap-2">
-      <img className="h-24 w-24" src="icons/brand.png" />
-      <span className="font-logo text-2xl text-amber-100">Goblin Finder</span>
+    <div className={`flex items-center justify-center gap-2 ${className}`}>
+      <img className={styles[type].img} src="icons/brand.png" />
+      <span className="font-logo text-3xl text-amber-100">Goblin Finder</span>
     </div>
   );
 }
